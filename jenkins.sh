ce42e7b7efadaf435e2f0f0004161f3c2b46a47b
Kurt Madel <kmadel@mac.com>
2017-05-13 10:40:44 -0400

    moved jvm settings back to Dockerfile BEEDEMO_JAVA_OPTS

--- jenkins.sh
+++ jenkins.sh
@@ -52,7 +52,7 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  eval "exec java  -server -XX:+AlwaysPreTouch XX:+UseConcMarkSweepGC $BEEDEMO_JAVA_OPTS $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
+  eval "exec java $BEEDEMO_JAVA_OPTS $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
