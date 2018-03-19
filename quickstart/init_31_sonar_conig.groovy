419b5c3a240cb32a628eaed1cefde1840e73d6f1
Kurt Madel <kmadel@mac.com>
2018-03-19 16:44:34 -0400

    added sonar plugin and sonarqube config

---
+++ quickstart/init_31_sonar_conig.groovy
@@ -0,0 +1,43 @@
+import jenkins.model.Jenkins
+
+import com.cloudbees.plugins.credentials.CredentialsProvider
+import org.jenkinsci.plugins.plaincredentials.StringCredentials
+import com.cloudbees.plugins.credentials.CredentialsMatchers
+
+import hudson.plugins.sonar.SonarInstallation
+import hudson.plugins.sonar.SonarGlobalConfiguration
+import static hudson.plugins.sonar.utils.SQServerVersions.SQ_5_3_OR_HIGHER
+
+import java.util.logging.Logger
+
+String scriptName = "init_31_sonar_conig"
+
+Logger logger = Logger.getLogger(scriptName)
+
+File disableScript = new File(Jenkins.getInstance().getRootDir(), ".disable-$scriptName")
+if (disableScript.exists()) {
+    logger.info("DISABLE install plugins script")
+    return
+}
+
+logger.info("Begin Configuring Sonarqube")
+
+j = Jenkins.getInstance()
+
+//need to get credentials from CJOC as the sonar plugin doesn't support credentials
+StringCredentials cred = null
+
+logger.info("checking for provided credentials based on id of $credentialsId")
+
+List<StringCredentials> candidates = new ArrayList<StringCredentials>();
+candidates.addAll(CredentialsProvider.lookupCredentials(org.jenkinsci.plugins.plaincredentials.StringCredentials.class, j.instance));
+
+cred = CredentialsMatchers.firstOrNull(candidates, CredentialsMatchers.withId("sonar.beedemo"))
+
+sonarGlobalConfiguration = new SonarGlobalConfiguration()
+globalConfiguration.setInstallations(new SonarInstallation("beedemo", "https://sonar.k8s.beedemo.net", SQ_5_3_OR_HIGHER, cred.secret.plainText, null, null, null, null, null, null, null, null, null))
+
+logger.info("Done Configuring Sonarqube")
+
+//create marker file to disable scripts from running twice
+disableScript.createNewFile()
