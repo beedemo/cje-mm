701b3dae7c066e57ce60a0b9250385967c981262
Kurt Madel <kmadel@mac.com>
2018-04-29 19:07:57 -0400

    updates to quickstart/init_99_quickstart_save.groovy

--- quickstart/init_99_quickstart_save.groovy
+++ quickstart/init_99_quickstart_save.groovy
@@ -8,17 +8,17 @@ Logger logger = Logger.getLogger("init_99_quickstart_save.groovy")
     
 File disableSaveScript = new File(Jenkins.getInstance().getRootDir(), ".disable-init_99_quickstart_save-script")
 if (disableSaveScript.exists()) {
-    logger.info("DISABLED init_99_quickstart_save script")
+    logger.info("init_99_quickstart_save script has been disabled")
     return
 }
-Jenkins.getInstance().save()
-logger.info("saving config changes, configuration finished")
-
+logger.info("before thread for restart - restart in 90 seconds")
 Thread.start {
       logger.info("sleeping for 90 seconds before restart")
       sleep 90000
       logger.info("preparing to restart Jenkins")
       Jenkins.instance.restart()
 }
+Jenkins.getInstance().save()
+logger.info("saving config changes, configuration finished")
 
 new File(Jenkins.getInstance().getRootDir(), ".disable-init_99_quickstart_save-script").createNewFile()
