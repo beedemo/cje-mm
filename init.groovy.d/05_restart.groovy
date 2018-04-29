2839ac2fcbafbad2b60b437d4a87916c8d03ee89
Kurt Madel <kmadel@mac.com>
2018-04-29 17:22:50 -0400

    attempting to start thread to resart has temporary fix for team license issue

---
+++ init.groovy.d/05_restart.groovy
@@ -0,0 +1,21 @@
+import jenkins.model.Jenkins
+
+import java.util.logging.Logger
+
+Logger logger = Logger.getLogger("init.d 05_restart.groovy")
+
+
+
+File disableSaveScript = new File(Jenkins.getInstance().getRootDir(), ".disable-05_restart-script")
+if (disableSaveScript.exists()) {
+    logger.info("DISABLED init.d .disable-05_restart script")
+    return
+} 
+
+Thread.start {
+      sleep 360000
+      logger.info("preparing to restart Jenkins")
+      Jenkins.instance.restart()
+}
+
+new File(Jenkins.getInstance().getRootDir(), ".disable-05_restart-script").createNewFile()
